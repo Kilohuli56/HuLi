ac1488942d5ec28ffa4de80ea865d729f2fb9940
HuLi <77619516+Kilohuli56@users.noreply.github.com>
2021-03-23 20:06:22 +0800

    Update 運算.js

--- js/運算.js
+++ js/運算.js
@@ -1,22 +1,22 @@
- function add_1(){
+function add_1(){
         var x=document.getElementById("X").value;
         var y=document.getElementById("Y").value;
         var z=parseInt(x)+parseInt(y)
     document.getElementById("change1").innerHTML=z;}
 
-        function add_2(){
+function add_2(){
         var x=document.getElementById("X").value;
         var y=document.getElementById("Y").value;
         var z=parseInt(x)-parseInt(y)
     document.getElementById("change2").innerHTML=z;}
 
-        function add_3(){
+function add_3(){
         var x=document.getElementById("X").value;
         var y=document.getElementById("Y").value;
         var z=parseInt(x)parseInt(y)
     document.getElementById("change3").innerHTML=z;}
 
-        function add_4(){
+function add_4(){
         var x=document.getElementById("X").value;
         var y=document.getElementById("Y").value;
         var z=parseInt(x)/parseInt(y)
