ce200226332d4c013081fcd2a3294f28c76772d6
HuLi <77619516+Kilohuli56@users.noreply.github.com>
2021-03-23 19:53:24 +0800

    Add files via upload

---
+++ js/運算.js
@@ -0,0 +1,23 @@
+ function add_1(){
+        var x=document.getElementById("X").value;
+        var y=document.getElementById("Y").value;
+        var z=parseInt(x)+parseInt(y)
+    document.getElementById("change1").innerHTML=z;}
+
+        function add_2(){
+        var x=document.getElementById("X").value;
+        var y=document.getElementById("Y").value;
+        var z=parseInt(x)-parseInt(y)
+    document.getElementById("change2").innerHTML=z;}
+
+        function add_3(){
+        var x=document.getElementById("X").value;
+        var y=document.getElementById("Y").value;
+        var z=parseInt(x)parseInt(y)
+    document.getElementById("change3").innerHTML=z;}
+
+        function add_4(){
+        var x=document.getElementById("X").value;
+        var y=document.getElementById("Y").value;
+        var z=parseInt(x)/parseInt(y)
+    document.getElementById("change4").innerHTML=z;}
